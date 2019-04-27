6e77ea493b105172d196724c260296d4d6a0c461
Thomas <thomas@craftacademy.se>
2019-04-27 15:10:09 +0200

    adds react router and pages for about and projects listing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
+
+import { Switch, Route } from 'react-router-dom'
+
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Hello from './components/Hello'
+import About from './components/About'
+import Projects from './components/Projects'
+
+import './css/tailwind.css'
+
 const App = () => {
   return (
     <>
-      <Header />
-      <Hello />
-      <Footer />
+      <div className="min-h-screen">
+        <div className="flex flex-col w-full fixed pin-l pin-y">
+          <Header />
+          <div className="px-4 pt-4 flex-1 overflow-y-scrol">
+            <Switch>
+              <Route exact path="/" component={Hello} />
+              <Route exact path="/projects" component={Projects} />
+              <Route exact path="/about" component={About} />
+            </Switch>
+            {/* <Hello /> */}
+          </div>
+          <Footer />
+        </div>
+      </div>
+
     </>
   );
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+  render() {
+    return (
+      <div>
+        These are my projects....
+      </div>
+    );
+  }
+}
+
+export default Projects;
